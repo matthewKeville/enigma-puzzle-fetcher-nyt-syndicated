3cb8af4b1986f53824f09b5dfbf4aea6608a3062
matthewKeville <matthewkeville@protonmail.com>
2025-04-21 18:50:10 -0400

    rename plugin

--- constants.py
+++ constants.py
@@ -1,5 +1,5 @@
 import datetime
 VERSION = "0.1"
-PLUGIN_NAME = "SeattleTimesNYTPzzl"
+PLUGIN_NAME = "NYTSyn"
 DATE_MINIMUM = datetime.datetime(2016, 1, 1).date()
 SCHEMA_BASE_URL = "https://raw.githubusercontent.com/matthewKeville/enigma-puzzle-fetcher-schema/main/"
